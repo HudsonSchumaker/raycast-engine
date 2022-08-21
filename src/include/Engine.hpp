a5d778063a021d25d8a29776f2b9f19f45e8e28a
HudsonSchumaker <hudson.schumaker@me.com>
2022-08-21 13:14:38 +0200

    Changes

--- src/include/Engine.hpp
+++ src/include/Engine.hpp
@@ -8,12 +8,14 @@
 #include "Common.hpp"
 #include "Player.hpp" 
 #include "Map.hpp" 
+#include "Ray.hpp" 
 
 class Engine final {
 private:
     SDL_Renderer* renderer;
     Player player;
     Map map;
+    Ray rays[Common::NUM_RAYS];
 
     bool isRunning = false;
     int ticksLastFrame = 0;
@@ -25,6 +27,7 @@ private:
     void loop();
     void castRay(float rayAngle, int stripId);
     void castAllRays();
+    void renderRays();
     
 public:
     Engine(SDL_Renderer* renderer);
