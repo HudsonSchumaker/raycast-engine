3b263eff14a9b3ff35ed10a375ae580efd9d1f22
HudsonSchumaker <hudson.schumaker@me.com>
2022-08-22 21:13:38 +0200

    Add color buffer

---
+++ src/include/ColorBuffer.hpp
@@ -0,0 +1,23 @@
+/*
+    Windows 10/11
+    SchumakerTeam
+    Hudson Schumaker
+*/
+
+
+#pragma once
+#include "Common.hpp"
+
+class ColorBuffer final {
+public:
+    
+    
+    
+    
+    
+    ColorBuffer();
+    ~ColorBuffer() = default;
+
+
+};
+
