f90d48b10e9669249a5fae718786ef60406558cb
HudsonSchumaker <hudson.schumaker@me.com>
2022-08-23 19:21:44 +0200

    3d walls

--- src/ColorBuffer.cpp
+++ src/ColorBuffer.cpp
@@ -46,3 +46,6 @@ void ColorBuffer::render() {
     );
     SDL_RenderCopy(renderer, colorBufferTexture, NULL, NULL);
 }
+void ColorBuffer::setColorBuffer(int index, uint32_t color) {
+    colorBuffer[index] = color;
+}
